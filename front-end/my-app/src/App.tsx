8259f1c154bd0c4bf4abf25733f66911fe013a0c
mglaum <m.glaum@ufl.edu>
2023-02-07 18:34:48 -0500

    commit to fork

--- front-end/my-app/src/App.tsx
+++ front-end/my-app/src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home'
+import Navbar from './components/Navbar/NavbarElements';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. We have liftoff!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
-export default App;
+export default App; 
---
+++ front-end/my-app/src/components/Navbar/NavbarElements.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavBar'
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <NavLink to="/">
+                <img src={require('../../images/logo3.png')} alt="logo"/>
+            </NavLink>
+            <Bars /> 
+            <NavMenu>
+                <NavLink to='/about'>
+                     About 
+                </NavLink>
+                <NavLink to="/calendar">
+                     Calendar 
+                </NavLink>
+                <NavLink to="/tutoring">
+                     Tutoring
+                </NavLink>
+                <NavLink to="/internships">
+                     Internships
+                </NavLink>
+                <NavLink to="/sign-up">
+                     Sign Up 
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to="/signin">
+                     Sign In 
+                </NavBtnLink>
+            </NavBtn>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar; 
+
+// npm install styled-components
+// npm install react-router-dom react-icons
