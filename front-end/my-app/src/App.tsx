4de4a755ec5fbaaf1db3f323e58bf051cf18b13a
Savannah Fletcher <sfletcher777@gmail.com>
2023-02-08 16:18:59 -0500

    Merge branch 'main' of https://github.com/savannahfletcher/ChompSci

--- front-end/my-app/src/App.tsx
+++ front-end/my-app/src/App.tsx
@@ -1,19 +1,3 @@
-
-<<<<<<< HEAD
-
-}
-
-export default App;
-=======
-  return (
-    <Router>
-      <Navbar />
-    </Router>
-  );
-}
-
-export default App; 
->>>>>>> 53b84a2db0dd83e8b39df16c5d941b8d5b98fbec
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
--- front-end/my-app/src/pages/Signin-page.tsx
+++ front-end/my-app/src/pages/Signin-page.tsx
@@ -5,32 +5,24 @@ import { render } from 'react-dom';
 import LoginForm from './LoginForm';
 import './signin-style.css';
 
-export const Signin = () => {
+export const SignIn = () => {
   const onSubmitUsername = (username: string, password: string) => alert('Your Username: '+ username +'\nPassword: '+ password);
 
     return (
         <div>
             <Navbar></Navbar>
-            {/* <div style={{
-            display: "flex", 
-            justifyContent: "center", 
-            alignItems: "center",
-            height: "90vh"
-        }}>Sign-in Page</div> */}
+            
         <header className="Login-header">
           
           <LoginForm onSubmit ={onSubmitUsername} />
 
       </header>
 
-        {/* <h1> Savannah's code goes here</h1> */}
         </div>
 
     );
 };
 
-render(<Signin />, document.getElementById('root'));
+// render(<SignIn/>, document.getElementById('root')); //breaks everything for some reason....
 
-
-
-export default SignIn
+export default SignIn;
