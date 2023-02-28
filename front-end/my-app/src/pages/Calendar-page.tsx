fac42aeb43082966bdeabea965bd4cc85a72a70b
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-02-28 17:51:07 -0500

    Update Calendar-page.tsx

--- front-end/my-app/src/pages/Calendar-page.tsx
+++ front-end/my-app/src/pages/Calendar-page.tsx
@@ -9,7 +9,8 @@ const Calendar = () => {
             display: "flex", 
             justifyContent: "center", 
             alignItems: "center",
-            height: "90vh"
+            height: "90vh",
+            backgroundColor: "#282c34"
         }}>Calendar Page</div>
         </div>
 
