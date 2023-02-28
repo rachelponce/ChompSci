86744e5e1c3f54b227122339517be41030e72803
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-02-28 17:51:40 -0500

    Update Home-page.tsx

--- front-end/my-app/src/pages/Home-page.tsx
+++ front-end/my-app/src/pages/Home-page.tsx
@@ -11,7 +11,8 @@ const Home = () => {
             display: "flex", 
             justifyContent: "center", 
             alignItems: "center",
-            height: "90vh"
+            height: "90vh",
+            backgroundColor: "#282c34"
         }}>
             Home Page
             </div>
