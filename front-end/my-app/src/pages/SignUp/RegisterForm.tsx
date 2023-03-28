72265a5da3cd6fb095a510c4a7b0abc6a3d81cfe
Savannah Fletcher <sfletcher777@gmail.com>
2023-03-28 17:48:50 -0400

    More input fields for sign-up

--- front-end/my-app/src/pages/SignUp/RegisterForm.tsx
+++ front-end/my-app/src/pages/SignUp/RegisterForm.tsx
@@ -1,3 +1,4 @@
+import { ClassNames } from '@emotion/react';
 import React, { SyntheticEvent, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import './signup-style.css';
@@ -5,13 +6,17 @@ import './signup-style.css';
 export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
   const [isDisabled, setIsDisabled] = React.useState(true);
 
   async function handleSubmit(event: { preventDefault: () => void; }) {
     event.preventDefault();
-    onSubmit(username, password);
+    onSubmit(username, password, firstName, lastName);
     setUsername('');
     setPassword('');
+    setFirstName('');
+    setLastName('');
     setIsDisabled(true);
 
   // Useful for catching errors in console
@@ -26,6 +31,8 @@ export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
+        FirstName: firstName,
+        LastName: lastName,
         Username: username,
         Password: password
       })
@@ -36,16 +43,23 @@ export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
     console.log(content);
   }
 
+  function handleChangeFirstName(event: { target: { value: string};}) {
+    setFirstName(event.target.value);
+  }
+  function handleChangeLastName(event: { target: { value: string};}) {
+    setLastName(event.target.value);
+  }
+
   function handleChangeUsername(event: { target: { value: string; }; }) {
     setUsername(event.target.value.toLowerCase());
   }
 
   function handleChangePassword(event: { target: { value: string; }; }) {
-    setPassword(event.target.value.toLowerCase());
+    setPassword(event.target.value);
   }
 
   useEffect(() => {
-    if (password !== '' && username !== '') {
+    if (password !== '' && username !== ''&& firstName !== ''&& lastName !== '') {
       setIsDisabled(false);
     } else {
       setIsDisabled(true);
@@ -53,12 +67,28 @@ export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
   }, [username, password]);
 
   return (
-    //formatting of login page: username/input box,password/input box, button
     <form onSubmit={handleSubmit}>
-      <div>
+      <div className="signUpText">
       <div className="header">
-        {/* Sign Up: */}
         <p>Sign Up:</p>
+        </div>
+        <div>
+          <label htmlFor="first-name">First Name: </label>
+          <input
+            id="first-name"
+            type="text"
+            onChange={handleChangeFirstName}
+            value={firstName}
+          />
+        </div>
+        <div>
+          <label htmlFor="last-name">Last Name: </label>
+          <input
+            id="last-name"
+            type="text"
+            onChange={handleChangeLastName}
+            value={lastName}
+          />
         </div>
         <label htmlFor="username-input">UF Email: </label>
         <input
@@ -67,7 +97,7 @@ export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
           onChange={handleChangeUsername}
           value={username}
         />
-      </div>
+      
       <div>
         <label htmlFor="password-input">Password: </label>
         <input
@@ -78,11 +108,13 @@ export const RegisterForm = ({ onSubmit }: {onSubmit:any}) => {
         />
         <p></p>
       </div>
+      
       <header className="button">
       <button id="login-button" type="submit" disabled={isDisabled}>
         Submit
       </button>
       </header>
+      </div>
     </form>
   );
 };
