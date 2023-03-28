922a2a6eb9b30a668126408f3a6144eced7f7be7
mglaum <m.glaum@ufl.edu>
2023-03-28 11:56:41 -0400

    Functional slider for home page

--- front-end/my-app/src/pages/Home/Home-page.tsx
+++ front-end/my-app/src/pages/Home/Home-page.tsx
@@ -1,54 +1,25 @@
 import React, { Component, useState } from 'react';
 import "./Home-page.css"
 import ReactDOM from 'react-dom';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Typewriter from "typewriter-effect"; 
-var Carousel = require('react-responsive-carousel').Carousel; 
-
+import ImageSlider from './Slider/Slider';
+import { SliderData } from './Slider/SliderData';
+  
 const Home = () => {
-    const [state] = useState ({
-        title: "Welcome to ", 
-        titleTwo: "chompSci", 
-        titleThree: "A place for UF Computer Science ",
-        image: "gator.gif"
-    }); 
 
     return(
         <>
-        <div className="home">
-            <div className="home-intro">
-                <h2>
-                    <div className='title'> {state.title}</div>
-                    <div className='titleTwo'> {state.titleTwo}</div>
-                    <div className='titleThree'> {state.titleThree}</div>
-                </h2>
-                <div className='text'>
-                    <Typewriter
-                        options={{
-                            autoStart: true,
-                            loop: true,
-                            delay: 40,
-                            strings: [
-                                "students",
-                                "clubs",
-                                "tutors",
-                                "recruiters",
-                                "professors",
-                                "organizations"
-                            ],
-                        }} />
-                </div>
-            </div>
-            <div className='home-image'>
-            <img src={require('../../images/gator.gif')} alt="gator"/>
-            </div>
-        </div>
         <div className="whats-new">
-            <h1>New section</h1>
+            <div className='whats-new-text'>
+                Happening this week at UF:
+             </div>
         </div>
-        </>
+
+        <div>
+             <ImageSlider slides={SliderData} />
+        </div>
+        </> 
     )
-};
+}
 
 export default Home; 
 
---
+++ front-end/my-app/src/pages/Home/Slider/SliderData.tsx
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+      image: require("../home-images/pic1.png"), 
+    },
+    {
+      image: require("../home-images/pic2.png"), 
+    },
+    {
+        image: require("../home-images/pic3.png"), 
+    },
+  ];
