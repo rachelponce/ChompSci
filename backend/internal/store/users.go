bf3db890eb442398e8d7a86424b3703708c7be90
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-03-29 12:04:59 -0400

    Updated Verification Func.

--- backend/internal/store/users.go
+++ backend/internal/store/users.go
@@ -10,12 +10,11 @@ import (
 
 type User struct {
 	gorm.Model
-	//Username string
 	FirstName string `validate:"required,min=2"`
-	LastName string `validate:"required,min=2"`
-	Email string `validate:"required,email"`
-	Password string `gorm:"not null"`
-	UserType int
+	LastName  string `validate:"required,min=2"`
+	Email     string `gorm:"uniqueIndex"`
+	Password  string `gorm:"uniqueIndex"`
+	UserType  int
 	// CreatedAt
 	// UpdatedAt
 	// DeletedAt
@@ -37,7 +36,7 @@ func UpdateTable(FirstName string, LastName string, Email string, Password strin
 
 	// Create
 	//db.Create(&User{Username: Username, Password: Password})
-	db.Create(&User{FirstName: FirstName, LastName:LastName, Email:Email, Password: Password, UserType: UserType})
+	db.Create(&User{FirstName: FirstName, LastName: LastName, Email: Email, Password: Password, UserType: UserType})
 
 	var user User
 	db.First(&user, 1) // first row in table ordered by ID
@@ -48,17 +47,16 @@ func UpdateTable(FirstName string, LastName string, Email string, Password strin
 }
 
 // verifying Email, since duplicate names are possible
-func Verification(FirstName string, Password string) error {
+func Verification(Email string, Password string) error {
 	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// need to adjust to check Email
 	var user User
-	db.First(&user, "username = ?", FirstName)
+	db.First(&user, "email = ?", Email) // Find user with email provided in parameter
 
-	if err := db.Where("username = ?", FirstName).First(&user).Error; err != nil {
+	if err := db.Where("email = ?", Email).First(&user).Error; err != nil {
 		return fmt.Errorf("Invalid login credentials provided", err)
 	}
 
