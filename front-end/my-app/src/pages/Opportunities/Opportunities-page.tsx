c2c26d1cb74694c8fea8c4beeb22454101d2ac05
Savannah Fletcher <sfletcher777@gmail.com>
2023-04-02 18:37:16 -0400

    Updated tutoring to opportunities page

--- front-end/my-app/src/pages/Opportunities/Opportunities-page.tsx
+++ front-end/my-app/src/pages/Opportunities/Opportunities-page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar-elements'
 
-const Tutoring = () => {
+const Opportunities = () => {
     return (
         <div>
             {/* <Navbar></Navbar> */}
@@ -11,10 +11,10 @@ const Tutoring = () => {
             alignItems: "center",
             height: "90vh",
             backgroundColor: "#282c34"
-        }}><h1>Tutoring Page</h1></div>
+        }}><h1>Opportunities Page</h1></div>
         </div>
 
     )
 }
 
-export default Tutoring
+export default Opportunities
