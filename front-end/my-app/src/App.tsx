08467f36ef366eb3a125587ec1d929f51ed9d3e0
mglaum <m.glaum@ufl.edu>
2023-04-16 19:29:21 -0400

    Work on input for calendar

--- front-end/my-app/src/App.tsx
+++ front-end/my-app/src/App.tsx
@@ -11,6 +11,7 @@ import Landing from './pages/Landing/Landing'
 import NavbarNSI from './components/Navbar/Navbar-NotSignedIn';
 import NavbarSI from './components/Navbar/Navbar-SignedIn';
 import Footer from './components/Footer/Footer'
+import AddEvent from './pages/Calendar/Add-Event';
 // import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         <Route path="/calendar" element={ <Calendar/> }></Route>
         <Route path="/signin" element={ <SignIn/> }></Route>
         <Route path="/signup" element={ <SignUp/> }></Route>
+        <Route path="/calendar/add-event" element={ <AddEvent/> }></Route>
       </Routes>
       <Footer />
     </Router>
