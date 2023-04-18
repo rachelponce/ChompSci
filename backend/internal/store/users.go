d5960010699754dbf0018301c4551aad86cdc154
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-04-18 16:36:32 -0400

    Added functions to access user info from database

--- backend/internal/store/users.go
+++ backend/internal/store/users.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"fmt"
+	"strconv"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"github.com/gin-gonic/gin"
@@ -26,6 +27,33 @@ var Users []*User
 
 func PrintUserInfo(FirstName string, LastName string, Email string, Password string, UserType int) {
 	fmt.Println(FirstName, LastName, Email, Password, UserType)
+}
+
+func PrintSignIn(Email string, Password string) {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+
+	if err := db.Where("email = ? AND password = ?", Email, Password).First(&user).Error; err != nil {
+		return
+	}
+
+	fmt.Println("First Name: ", user.FirstName)
+	fmt.Println("Last Name: ", user.LastName)
+	fmt.Println("Email Name: ", user.Email)
+
+	if user.UserType == 1 {
+		fmt.Println("User Type: Student")
+	}
+	if user.UserType == 2 {
+		fmt.Println("User Type: Faculty")
+	}
+	if user.UserType == 3 {
+		fmt.Println("User Type: Club Board Member")
+	}
 }
 
 func UpdateTable(FirstName string, LastName string, Email string, Password string, UserType int) {
@@ -61,18 +89,58 @@ func Verification(Email string, Password string) bool {
 		return false
 	}
 
-	/*
-		db.First(&user, "email = ?", Email) // Find user with email provided in parameter
-
-		if err := db.Where("email = ?", Email).First(&user).Error; err != nil {
-			return false
-		}
-	*/
-
 	println("User found")
 	return true
 }
 
+// Functions to retrieve specific parts of user info from database
+func First(Email string) string {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+
+	db.First(&user, "email = ?", Email)
+	return user.FirstName
+}
+
+func Last(Email string) string {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+
+	db.First(&user, "email = ?", Email)
+	return user.LastName
+}
+
+func Email(Email string) string {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+
+	db.First(&user, "email = ?", Email)
+	return user.Email
+}
+
+func UserType(Email string) string {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+
+	db.First(&user, "email = ?", Email)
+	return strconv.Itoa(user.UserType)
+}
 
 func testSetup() {
 	gin.SetMode(gin.TestMode)
