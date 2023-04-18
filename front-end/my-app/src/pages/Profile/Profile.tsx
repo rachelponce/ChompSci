0f12b547ecae1aec55bbc73fd33554a76cb38cdb
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-04-18 16:46:40 -0400

    Updated Profile Page to include default user info

--- front-end/my-app/src/pages/Profile/Profile.tsx
+++ front-end/my-app/src/pages/Profile/Profile.tsx
@@ -1,19 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './Profile.css'
+import LoginForm from '../SignIn/LoginForm';
+import SignIn from '../SignIn/Signin-page';
 
 const Profile = () => {
-  return (
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [userType, setUserType] = useState('');
+
+  // http://localhost:3000/api/user/:userid
+  // http://localhost:8080/api/user
+
+  useEffect(() => {
+    (
+      async () => {
+        const response = await fetch('http://localhost:3000/api/user/:userid', {
+          headers: {'Content-Type': 'application/json'},
+          credentials: 'include',
+        });
+
+        const content = await response.json();
+
+        setName(content.name);
+        setEmail(content.email);
+        setUserType(content.userType);
+
+      }
+    )();
+  });
+
+
+return (
     <div className='profile'>
         <div className='heading'>My Profile</div>
-        Name: 
+
         <p></p>
-        Email: 
+          {name ? 'Name: ' + name : ' '}
         <p></p>
-        User Type:
+        
+        <p></p>
+        {email ? 'Email: ' + email : ' '} 
         <p></p>
 
+        <p></p>
+          {userType ? 'User Type: ' + userType : ' '} 
+        <p></p>
+
+        <header className='button'>
+          <button className="full-rounded" id="login-button">
+          Update Profile
+          <div className="border full-rounded"></div>
+          </button>
+        </header>
+
     </div>
-  )
-}
+  );
+};
+
 
 export default Profile
