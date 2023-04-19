6a62aa0a9265b7b01a6bc5b936cab14e1d2b747b
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2023-04-19 18:14:22 -0400

    Unit testing for user info components from db.

--- backend/internal/store/database_test.go
+++ backend/internal/store/database_test.go
@@ -27,3 +27,71 @@ func TestVerification(t *testing.T) {
 		t.Errorf("Correct user info was not found in database.")
 	}
 }
+
+// Testing First function which will retrieve user's first name from database
+func TestFirst(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	got := First("rachel.ponce@ufl.edu")
+	want := "Rachel"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// Testing Last function which will retrieve user's last name from database
+func TestLast(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	got := Last("rachel.ponce@ufl.edu")
+	want := "Ponce"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// Testing Email function which will retrieve user's email from database
+func TestEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	got := Email("rachel.ponce@ufl.edu")
+	want := "rachel.ponce@ufl.edu"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// Testing UserType function which will retrieve user's type from database
+func TestUserType(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("userInfo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	got := UserType("rachel.ponce@ufl.edu")
+	want := "1"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
